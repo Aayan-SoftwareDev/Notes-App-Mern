0b157305a63272f4e77bc7f52eb0b0dd4adfa5c9
Aayan <aayan.softwaredev@gmail.com>
2026-01-16 20:00:56 +0500

    removed

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,10 +24,6 @@ app.use("/api/notes", notesRoutes);
 
 if(process.env.NODE_ENV == "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-    app.get("/:path(.*)", (_, res) => {
-        res.sendFile(__dirname, "../frontend", "dist", "index.html");
-});
 }
 
 connectDB().then(() => {
