fd3b47b7cbefdb40257f5ad7d85a0a6907e8f9ad
Aayan <aayan.softwaredev@gmail.com>
2026-01-16 19:47:56 +0500

    added new wildcard syntax

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,7 +25,7 @@ app.use("/api/notes", notesRoutes);
 if(process.env.NODE_ENV == "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (_, res) => {
+    app.get("/:path*", (_, res) => {
         res.sendFile(__dirname, "../frontend", "dist", "index.html");
 });
 }
